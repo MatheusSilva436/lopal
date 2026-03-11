2213bd366e7967a14927e7dbaaca19da40641c2c
MatheusSilva436 <msilvacosta516@gmail.com>
2026-03-11 09:19:13 -0300

    feat: Opções de encerramento da calculadora

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -3,31 +3,43 @@ function calculadora(){
         let resultado = 0.0;
         let operando1;
         let operando2;
-        let operandor;
+        let operador;
 
-        operando1 = prompt("Digite o primeiro número: ");
-        operando2 = prompt("Digite o segundo número: ");
-        operador = prompt("Digite uma das operações (+ - * / ): ");
+        operando1 = parseFloat( prompt("Digite o primeiro número: ") );
+        operando2 = parseFloat( prompt("Digite o segundo número: ") );
+        operador = prompt("Digite uma das operações (+ - * / ): ") ;
 
         if ( operador === "+"){
-            resultado = operando1 + operando2;
+            resultado = operando1 + operando2
         } else if ( operador === "-" ){
             resultado = operando1 - operando2
         } else if ( operador === "*" ){
             resultado = operando1 * operando2
         } else if ( operador === "/" ){
-            if (operando2== 0){
-                alert("Não é possível dividir por zero")
-                return
+            if (operando2 == 0){
+                alert("Não é possível dividir por zero");
+                continua = prompt("Digite sim para continuar e não encerrar.")
+                if( continua === "não"){
+                    return
+                }
+            } else {
+                resultado = operando1 / operando2;
             }
-            resultado = operando1 / operando2
+            
         } else {
             alert ("operador inválido!");
-            return
+            operador_valido = false
+            continua = prompt("Digite sim para continuar e não para encerrar")
+            if ( continua == "não"){
+                    return
+                }
+            
         }
-
-
+        if( operador != "/" ||  operando2 != 0 ){
         alert("Resultado: " + 
             operando1 + " " + operador + " " + operando2 + " = " + resultado);
+        }
+    
+    
     }
 }    
